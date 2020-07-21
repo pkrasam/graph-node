772c144e4944cb52813815e4ae591ec2a95a38d6
Leonardo Yvens <leoyvens@gmail.com>
2020-07-21 12:21:33 -0300

    server: Refactor `run_subscription` as an async fn

--- server/http/src/service.rs
+++ server/http/src/service.rs
@@ -520,7 +520,10 @@ mod tests {
             ))))
         }
 
-        fn run_subscription(&self, _subscription: Subscription) -> SubscriptionResultFuture {
+        async fn run_subscription(
+            self: Arc<Self>,
+            _subscription: Subscription,
+        ) -> Result<SubscriptionResult, SubscriptionError> {
             unreachable!();
         }
 
